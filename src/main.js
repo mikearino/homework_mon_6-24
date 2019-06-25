d8e0722a678e502d4d6aa17ac2494ff114f69562
Mike Lofgren <mikeylofgren@gmail.com>
2019-06-24 19:32:38 -0700

    add front end dependencies.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { template } from './template';
 import './styles.css';
 import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function() {
   $('#template-form').submit(function(event) {
